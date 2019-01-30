874a07872f52332bbefa8e18fa273904a8f88d12
Sebastian Górski <lambda.zielony@protonmail.ch>
2019-01-30 20:37:55 +0100

    light effects added

--- program_obsluga_przyciskow/program_obsluga_przyciskow.ino
+++ program_obsluga_przyciskow/program_obsluga_przyciskow.ino
@@ -15,7 +15,7 @@
 
 #define DOUT A4
 #define CLK A5
-#define PIN A6
+#define PIN 2
 #define LICZBADIOD 8 // 240
 
 SoftwareSerial SerialtoLED(A2, A1); //komunikacja z ledami
@@ -35,7 +35,7 @@ void setup() {
   scale.tare(); //Reset the scale to 0
   long zero_factor = scale.read_average(); //Get a baseline reading
 
-  pixels.begin(); // Inicjalizacja
+
   for (int i = 0; i < LICZBADIOD; i++)
     pixels.setPixelColor(i, 128, 128, 128);
   pixels.show();
@@ -50,6 +50,8 @@ void setup() {
   pinMode(NEPTUNE, INPUT_PULLUP);
   pinMode(MOON, INPUT_PULLUP);
   pinMode(SUN, INPUT_PULLUP);
+
+  pixels.begin(); // Inicjalizacja
 
 }
 
@@ -77,55 +79,94 @@ void loop() {
   if (planetState == 1) {
     SerialtoLED.print("1");
     Serial.print("1");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 219, 206, 202);
+    pixels.show();
   }
   else if (planetState == 2) {
     SerialtoLED.print("2");
     Serial.print("2");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 199, 60, 7);
+    pixels.show();
   }
   else if (planetState == 3) {
     SerialtoLED.print("3");
     Serial.print("3");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 11, 255, 30);
+    pixels.show();
   }
   else if (planetState == 4) {
     SerialtoLED.print("4");
     Serial.print("4");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 233, 107, 57);
+    pixels.show();
   }
   else if (planetState == 5) {
     SerialtoLED.print("5");
     Serial.print("5");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 157, 91, 3);
+    pixels.show();
   }
   else if (planetState == 6) {
     SerialtoLED.print("6");
     Serial.print("6");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 255, 229, 180);
+    pixels.show();
   }
   else if (planetState == 7) {
     SerialtoLED.print("7");
     Serial.print("7");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 0, 127, 255);
+    pixels.show();
   }
   else if (planetState == 8) {
     SerialtoLED.print("8");
     Serial.print("8");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 3, 0, 253);
+    pixels.show();
   }
   else if (planetState == 9) {
     SerialtoLED.print("9");
     Serial.print("9");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 219, 176, 239);
+    pixels.show();
   }
   else if (planetState == 0) {
     SerialtoLED.print("0");
     Serial.print("0");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 255, 192, 0);
+    pixels.show();
+  } else if (planetState == 100){
+    SerialtoLED.print("e");
+    Serial.print("100");
+    for(int i=0; i<LICZBADIOD; i++)
+      pixels.setPixelColor(i, 128, 128, 128);
+    pixels.show();
   }
+
+  Serial.print(" ");
 
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
   for (int i = 0; i < 3; i++) 
-    rejestr[i] = (int)abs(scale.get_units());
+      rejestr[i] = (int)abs(scale.get_units());
   
   for (int i = 0; i < 3; i++) 
     srednia += rejestr[i];
   
   srednia /= 4;
   Serial.print(srednia);
+
   Serial.print(",");
 
-  delay(100);
+  delay(50);
 }
